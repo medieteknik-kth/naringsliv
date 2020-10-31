d1cb9392e833c3198f19d223461e1383802f9cab
Kristina Andersson <kristina.andersson@mail.ru>
2020-10-31 21:05:03 +0100

    Revert "delete all files"
    This reverts commit e1f5dcc8d6699250c7e6b14b690f8f306cb4f8a4.

---
+++ naringsliv2/src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "./node_modules/react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <button>click here</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
